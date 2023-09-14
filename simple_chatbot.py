be55679f84213ec92aa3c6d29d4adc091df34f09
karameruuuunnnn <71586392+karameruuuunnnn@users.noreply.github.com>
2023-09-14 09:40:06 +0900

    Update simple_chatbot.py

--- simple_chatbot.py
+++ simple_chatbot.py
@@ -2,24 +2,20 @@ import openai
 openai.api_key = "**********"
 
 def Ans_ChatGPT(question):
-    messages = [
-        {
+    messages = [{
             "role": "user",
             "content": question,
-        }
-    ]
-
+        }]
+    
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
 
     response = completion.choices[0].message['content']
-
     return response
 
 question = "hi"
-
 answer = Ans_ChatGPT(question)
 
 print(answer)
